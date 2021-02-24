ce65798d66901777550c4b006e472b6350f631f1
yosepjeon <enekelx1@naver.com>
2021-02-24 21:54:55 +0900

    스프링이 사랑한 디자인패턴: Adapter Pattern

---
+++ src/main/java/com/yosep/spring/basic/pattern/adapter/noAdapter/ServiceA.java
@@ -0,0 +1,7 @@
+package com.yosep.spring.basic.pattern.adapter.noAdapter;
+
+public class ServiceA {
+    public void runServiceA() {
+        System.out.println("ServiceA");
+    }
+}
---
+++ src/main/java/com/yosep/spring/basic/pattern/adapter/noAdapter/ServiceB.java
@@ -0,0 +1,7 @@
+package com.yosep.spring.basic.pattern.adapter.noAdapter;
+
+public class ServiceB {
+    public void runServiceB() {
+        System.out.println("ServiceB");
+    }
+}
