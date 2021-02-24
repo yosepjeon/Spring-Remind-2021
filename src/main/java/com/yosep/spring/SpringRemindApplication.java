e5fd2cf49555126221b743b29393705d6ffefba6
yosepjeon <enekelx1@naver.com>
2021-02-24 21:25:50 +0900

    Init Project

---
+++ src/main/java/com/yosep/spring/SpringRemindApplication.java
@@ -0,0 +1,11 @@
+package com.yosep.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRemindApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRemindApplication.class);
+    }
+}
